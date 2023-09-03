eb87fb82da4b03f5603d0cd3d7bbeb729962edae
Leoj925 <leonardo.ortega992@gmail.com>
2023-09-02 22:03:54 -0300

    validacion

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 const express =require("express")
 const app=express()
 const path= require("path")
+const bodyParser= require("body-parser")
+const { body, validationResult} = require("express-validator")
+app.use(bodyParser.urlencoded({ extended:true}))
+const bcrypt = require("bcrypt")
+const cookieParser = require('cookie-parser');
+const fs = require("fs");
+
 
 const PORT=3000
 
@@ -9,7 +16,14 @@ app.set("view engine", "ejs");
 
 app.listen(PORT, ()=>console.log("Escuchando al puerto: ",PORT))
 
-app.use(express.static("public")) //de donde saco los archivos (carpeta publica =>muchos archivos distintos, estaticos)
+app.use(express.static("public")) 
+app.use(cookieParser())
+
+const read = () => JSON.parse(fs.readFileSync('./users.json', 'utf-8'))
+const writeJSON = (data) => {
+    let stringifiedData = JSON.stringify (data, null, 2);
+    return fs.writeFileSync("./users.json", stringifiedData);
+};
 
 /* RUTAS */
 app.get("/", (req,res)=> {
@@ -31,3 +45,39 @@ app.get("/cart", (req,res)=> {
 app.get("/register", (req,res)=> {
     res.render ("../views/register.ejs")
 })
+
+const validateUser= [
+    body("nombre").trim().notEmpty().isAlpha().withMessage("Solo caracteres alfabéticos"),
+    body("apellido").trim().notEmpty().isAlpha().withMessage("Solo caracteres alfabéticos"),
+    body("fechaNacim").trim().notEmpty().isDate().withMessage("Solo formato fecha"),
+    body("email").trim().notEmpty().isEmail().withMessage("Solo direcciones de email"),
+    body("emailConfirm").trim().notEmpty().isEmail().custom((value, { req }) => {
+        if (value !== req.body.email) {
+            throw new Error('Los correos electrónicos no coinciden');
+          }
+          return true;
+        }),
+    body("password").trim().notEmpty().isLength(8).withMessage("Minimo 8 caracteres alfanumericos"),
+    body("passwordConfirm").trim().notEmpty().isLength(8).custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('Las contraseñas no coinciden');
+          }
+          return true;
+        })
+];
+ 
+app.post("/register", validateUser, (req, res) => {
+            const errors = validationResult(req) 
+            if (!errors.isEmpty()) { 
+            console.log(errors)
+        }}
+        )
+
+    app.post("/register", (req,res) =>{
+        let oldJSON= read()
+        const newUser= {id: oldJSON.length +1 , nombre: req.body.nombre, apellido: req.body.apellido
+        };
+        oldJSON.push(newUser);
+        writeJSON(oldJSON);
+        res.redirect("/login");;
+      });
